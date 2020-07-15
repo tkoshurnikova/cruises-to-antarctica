34bbcce68c0342e8932b5ffd95f68020d414c936
Tanya <45432959+tkoshurnikova@users.noreply.github.com>
2020-07-15 16:36:22 +0300

    Исправления

---
+++ build/js/mobile-menu.js
@@ -0,0 +1,24 @@
+'use strict';
+
+(function () {
+  var pageHeader = document.querySelector('.page-header');
+  var headerToggle = pageHeader.querySelector('.page-header__nav-toggler');
+  var navBlock = pageHeader.querySelector('.page-header__nav');
+  var svgLogo = pageHeader.querySelector('.page-header__svg');
+
+  navBlock.classList.remove('page-header__nav--no-js');
+  headerToggle.classList.remove('page-header__nav-toggler--no-js');
+
+  if (pageHeader) {
+    headerToggle.addEventListener('click', function () {
+      headerToggle.classList.toggle('page-header__nav-toggler--menu-closed');
+      headerToggle.classList.toggle('page-header__nav-toggler--menu-opened');
+      navBlock.classList.toggle('page-header__nav--closed');
+      navBlock.classList.toggle('page-header__nav--opened');
+      svgLogo.classList.toggle('page-header__svg--menu-closed');
+      svgLogo.classList.toggle('page-header__svg--menu-opened');
+    });
+  }
+})();
+
+
---
+++ build/js/phone-validation.js
@@ -0,0 +1,20 @@
+'use strict';
+
+(function () {
+  var phoneInput = document.querySelector("#phone");
+
+  if (phoneInput) {
+    phoneInput.addEventListener('keydown', function (evt) {
+      var theEvent = evt || window.event;
+      var key = theEvent.keyCode || theEvent.which;
+      key = String.fromCharCode(key);
+      var regex = /[0-9]|\./;
+      if (!regex.test(key)) {
+        theEvent.returnValue = false;
+        if (theEvent.preventDefault) {
+          theEvent.preventDefault();
+        }
+      }
+    });
+  }
+})();
