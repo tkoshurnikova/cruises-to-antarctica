1cc7234c190352b47e6bc90d1c108cd7b41f8caa
Tanya <45432959+tkoshurnikova@users.noreply.github.com>
2020-07-16 11:35:05 +0300

    Исправления

--- source/js/mobile-menu.js
+++ source/js/mobile-menu.js
@@ -2,14 +2,15 @@
 
 (function () {
   var pageHeader = document.querySelector('.page-header');
-  var headerToggle = pageHeader.querySelector('.page-header__nav-toggler');
-  var navBlock = pageHeader.querySelector('.page-header__nav');
-  var svgLogo = pageHeader.querySelector('.page-header__svg');
-
-  navBlock.classList.remove('page-header__nav--no-js');
-  headerToggle.classList.remove('page-header__nav-toggler--no-js');
 
   if (pageHeader) {
+    var headerToggle = pageHeader.querySelector('.page-header__nav-toggler');
+    var navBlock = pageHeader.querySelector('.page-header__nav');
+    var svgLogo = pageHeader.querySelector('.page-header__svg');
+
+    navBlock.classList.remove('page-header__nav--no-js');
+    headerToggle.classList.remove('page-header__nav-toggler--no-js');
+
     headerToggle.addEventListener('click', function () {
       headerToggle.classList.toggle('page-header__nav-toggler--menu-closed');
       headerToggle.classList.toggle('page-header__nav-toggler--menu-opened');
